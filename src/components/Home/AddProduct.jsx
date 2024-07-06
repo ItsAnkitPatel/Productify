e4f04567f062533168e46452796f8600a05ce7dc
ItsAnkitPatel <itsakppatel@gmail.com>
2024-07-07 01:58:28 +0530

    refactor: add better responsiveness for phone screens

--- src/components/Home/AddProduct.jsx
+++ src/components/Home/AddProduct.jsx
@@ -34,10 +34,10 @@ const AddProduct = () => {
   return (
     <div className="absolute z-30 h-full w-full backdrop-blur-sm">
       {/* Positing the container in center of the page */}
-      <div className="flex min-h-96 w-9/12 flex-col items-center justify-center transition-all duration-700">
+      <div className="flex min-h-96 flex-col items-center justify-center transition-all duration-700 max-sm:mx-auto max-sm:w-9/12 md:w-10/12 lg:w-9/12">
         <div className="w-full drop-shadow-lg">
           <div className="mx-auto w-full overflow-hidden rounded-md bg-transparent p-2 px-5 shadow-xl shadow-gray-500/80 transition-all duration-700 sm:w-96 lg:mt-28">
-            <h1 className="mb-2 mt-5 text-3xl drop-shadow-none">
+            <h1 className="mb-2 mt-5 text-xl drop-shadow-none sm:text-3xl">
               Add Your Product
             </h1>
 
@@ -48,7 +48,7 @@ const AddProduct = () => {
                   name="product"
                   placeholder="Product Name"
                   required
-                  className="mt-2 h-12 rounded-md p-2 outline-none ring-gray-200 focus:ring-2"
+                  className="mt-2 h-10 rounded-md p-2 outline-none ring-gray-200 focus:ring-2 sm:h-12"
                   value={productField}
                   onChange={(e) => {
                     setProductField(e.target.value);
@@ -59,7 +59,7 @@ const AddProduct = () => {
                   name="price"
                   required
                   placeholder="Product Price in $"
-                  className="h-12 rounded-md p-2 outline-none ring-gray-200 focus:ring-2"
+                  className="h-10 rounded-md p-2 outline-none ring-gray-200 focus:ring-2 sm:h-12"
                   value={priceField}
                   onChange={(e) => {
                     setPriceField(e.target.value);
@@ -67,7 +67,7 @@ const AddProduct = () => {
                 />
 
                 <div className="mb-3 mt-2 flex w-full justify-between">
-                  <div className="flex w-full items-center justify-between px-px text-lg">
+                  <div className="flex w-full items-center justify-between px-px text-sm max-sm:gap-2 sm:text-lg">
                     <button
                       className="rounded-md bg-white px-3 py-2 text-black transition-all duration-300 hover:shadow-lg active:translate-y-2"
                       onClick={handleCancel}
