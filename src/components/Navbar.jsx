46d24db01f95d662c3051e3e976b949cfe6ab2e4
ItsAnkitPatel <itsakppatel@gmail.com>
2024-07-06 11:08:44 +0530

    refactor: Add link in the navbar to navigate to Hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import ButtonStyle from "./ButtonStyle";
 const Navbar = () => {
@@ -14,7 +14,7 @@ const Navbar = () => {
   };
   return (
     <div className="flex items-center justify-between">
-      <h1 className="text-3xl">Productify</h1>
+      <h1 className="text-3xl"><Link to={""}>Productify</Link></h1>
       {isUserLoggedIn && (
         <div>
           <ButtonStyle onClick={handleLogout}>Logout</ButtonStyle>
