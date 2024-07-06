435a023985184514a7ac5a28e2a3f2d5f4511946
ItsAnkitPatel <itsakppatel@gmail.com>
2024-07-06 21:25:12 +0530

    refactor: updated Hero component according to screen sizes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,27 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col min-h-96 w-full justify-center items-center">
-        <h1 className="text-8xl">Welcome to <span className="text-zinc-500">Productify</span></h1>
+    <div className="flex min-h-96 w-full flex-col items-center justify-center">
+      <h1 className="w-full text-center text-4xl transition-all duration-700 sm:text-nowrap md:text-6xl lg:text-7xl xl:text-8xl">
+        Welcome to <span className="text-zinc-500">Productify</span>
+      </h1>
 
-        <p className="text-2xl mt-4">The best place to manage your products</p>
+      <p className="mt-4 text-base text-center md:text-xl lg:text-2xl transition-all duration-700">
+        The best place to manage your products
+      </p>
 
-        <span className="text-zinc-500 mt-6">Please <Link to={"/login"} className="hover:underline underline-offset-2 text-zinc-700">Login</Link> First if you haven't</span>
+      <span className="mt-6 text-sm md:text-base text-center text-zinc-500 transition-all duration-700">
+        Please{" "}
+        <Link
+          to={"/login"}
+          className="text-zinc-700 underline-offset-2 hover:underline"
+        >
+          Login
+        </Link>{" "}
+        First if you haven't
+      </span>
     </div>
-  )
-}
-export default Hero
+  );
+};
+export default Hero;
